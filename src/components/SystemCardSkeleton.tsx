78319a43ddea89f36de83fb72f735ec33949afc3
Kenneth Vu <kvu@it.ucla.edu>
2026-02-19 02:50:04 -0800

    (TESTING) update skeleton styling

--- src/components/SystemCardSkeleton.tsx
+++ src/components/SystemCardSkeleton.tsx
@@ -1,16 +1,16 @@
 export const SystemCardSkeleton = () => {
     return (
-        <div className="card bg-base-200 animate-pulse-fast">
+        <div className="card bg-base-200">
             <div className="card-body p-4">
                 <div className="flex gap-4">
                     <div className="flex-shrink-0 flex gap-2">
-                        <div className="w-48 h-48 rounded-lg bg-base-300"></div>
-                        <div className="w-48 h-48 rounded-lg bg-base-300"></div>
-                        <div className="w-48 h-48 rounded-lg bg-base-300"></div>
+                        <div className="w-48 h-48 rounded-lg bg-base-300 animate-shimmer"></div>
+                        <div className="w-48 h-48 rounded-lg bg-base-300 animate-shimmer"></div>
+                        <div className="w-48 h-48 rounded-lg bg-base-300 animate-shimmer"></div>
                     </div>
                     <div className="flex-1 min-w-0 space-y-2">
-                        <div className="h-6 bg-base-300 rounded w-3/4"></div>
-                        <div className="h-3 bg-base-300 rounded w-1/2"></div>
+                        <div className="h-6 bg-base-300 rounded w-3/4 animate-shimmer"></div>
+                        <div className="h-3 bg-base-300 rounded w-1/2 animate-shimmer"></div>
                     </div>
                 </div>
             </div>
